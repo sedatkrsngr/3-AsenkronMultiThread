3a78bd36a234524c4b22756a2ee03fe4ae9fe8f7
sedat karasungur <limitless_fearless@hotmail.com>
2021-03-31 16:53:49 +0300

    ContinueWith Bitti

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskConsoleApp
+{
+    class Program
+    {
+       private async static  Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(calis);//static sınıftan statşc sınıf çağırılabilir
+
+            Console.WriteLine("arada yapılacak işler");
+
+            await myTask;
+
+        }
+
+        public  static void calis(Task<string> data)
+        {
+            Console.WriteLine(data.Result.Length);//Result Task<string> sonuclarını almak için kullanılır.
+        }
+    }
+}
