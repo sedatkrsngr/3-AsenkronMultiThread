8642812d3427288be82af051ad0f6dbee32267d4
sedat karasungur <limitless_fearless@hotmail.com>
2021-03-31 15:50:35 +0300

    Task Form Bitti

---
+++ TaskFormApp/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace TaskFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnread = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(28, 21);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(75, 23);
+            this.btnread.TabIndex = 0;
+            this.btnread.Text = "Dosya Oku";
+            this.btnread.UseVisualStyleBackColor = true;
+            this.btnread.Click += new System.EventHandler(this.btnread_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(28, 91);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(180, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(346, 21);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(87, 24);
+            this.btnCounter.TabIndex = 2;
+            this.btnCounter.Text = "sayac arttır";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(346, 51);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(122, 22);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(288, 91);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(180, 96);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 250);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnread;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ TaskFormApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskFormApp
+{
+    public partial class Form1 : Form
+    {
+        public int counter { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnread_Click(object sender, EventArgs e)
+        {
+            string data = string.Empty;
+            // string data = ReadFile(); senkron
+
+
+
+              Task<string> okuma =  ReadFileAsync(); // asenkton kullanınca metotda async yok sonradan ekledik await kullanana kadar thread kullanılmıyor şuan sadece atama yaptık Task söz vermek gibi düşünebiliriz. Yani o veriyi mutlaka sana döndürecek biz sadece awaitle çağırırız Eğer arada başka işler yaptıracaksak await ile çağırmayı daha sonra yaparız diğer işlemleri yaptıktan sonra await ile ilgili atamaları gerçekleştiririz
+            
+            richTextBox2.Text = await new HttpClient().GetStringAsync("https://www.google.com");//Burada bir işlem yapsın diye bir threadı meşgul ettik
+            
+            data = await okuma;
+            
+            richTextBox1.Text = data;
+            
+        }
+
+        private void btnCounter_Click(object sender, EventArgs e)//bunu koymamızın sebebi işlem olurken sayaç işlemini gerçekleştirmeize izin veriyor mu kontrol etmek için  senkronda izin vermiyor işlem bitene kadar ardından eğer işlem zamanında dokunduysak senkron bitince sayactaki bastığımız kadar attırımı yapar ama asenkronda işlem sürse dahi biz yan tarafta arttırma işlemini devam ettiriyoruz
+        {
+            textBoxCounter.Text = counter++.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string ReadFile()//senkron kodumuz
+        {
+            string data = string.Empty;
+            using (StreamReader reader = new StreamReader("dosya.txt"))//kalıtım aldığı sınıf IDisposable İnterfacesinden kalıtım aldığı için using kullanabilriz. İşlem bitince bellekten atılır dosya.text bin/debug içerisine eklendi
+            {
+                Thread.Sleep(5000);// İşlem yapılınca 5  sn bekletelim ki işlemimiz 5 sn sürüyormuş gibi olsun
+                data = reader.ReadToEnd();
+            }
+            return data;
+        }
+        private async Task<string> ReadFileAsync()//asenkron kodumuz
+        {
+            string data = string.Empty;
+            using (StreamReader reader = new StreamReader("dosya.txt"))//kalıtım aldığı sınıf IDisposable İnterfacesinden kalıtım aldığı için using kullanabilriz. İşlem bitince bellekten atılır dosya.text bin/debug içerisine eklendi
+            {
+ 
+               Task<string> myTask =  reader.ReadToEndAsync();//şuan thread meşgul değil await kullanıldığı an bir alt satıra geçmez
+
+                await Task.Delay(5000);// İşlem yapılınca 5  sn bekle
+                data = await myTask;//wait burada işlem tamamlanana kadar bekle demek alt satıra geçme demek
+            }
+            return  data;
+        }
+    }
+}
