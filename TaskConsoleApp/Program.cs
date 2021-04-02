2bab2bad118c7f2948ff77ec000ccaeab92cc89e
sedat <sedatkarasungur4@gmail.com>
2021-04-02 12:07:07 +0300

    WhenAll Metotu Bitti

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,26 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
-    class Program
+
+    public class Content 
     {
-       private async static  Task Main(string[] args)
+        public string Site { get; set; }
+        public int Len { get; set; }
+    }
+    internal class Program
+    {
+        private async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Main threadı :" + Thread.CurrentThread.ManagedThreadId);// Çalışan threadı görürüz
 
-            var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(calis);//static sınıftan statşc sınıf çağırılabilir
+            List<string> urlList = new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com",
+                "https://www.n11.com",
+                "https://www.haberturk.com"
+            };
 
-            Console.WriteLine("arada yapılacak işler");
+            List<Task<Content>> tastList = new List<Task<Content>>();
 
-            await myTask;
+            urlList.ToList().ForEach(x =>
+            {
+                tastList.Add(GetContentAsync(x));
+            });
 
+            var contents = await Task.WhenAll(tastList.ToArray());//Content[] dizi döner işlemler tamamlanırsa alta geçer await kullanmasaydık atamam Task<Content[]> şeklinde olacaktır.
+            //WhenAll tüm işlemleri gerçekleştir demek
+
+            contents.ToList().ForEach(x => {
+
+                Console.WriteLine($"{x.Site} boyut : {x.Len}");
+            });
         }
 
-        public  static void calis(Task<string> data)
+        public async static Task<Content> GetContentAsync(string url)
         {
-            Console.WriteLine(data.Result.Length);//Result Task<string> sonuclarını almak için kullanılır.
+            Content content = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
+
+            content.Site = url;
+            content.Len = data.Length;
+
+            Console.WriteLine("GetContentAsync threadı :" + Thread.CurrentThread.ManagedThreadId);// Çalışan threadı görürüz
+
+            return content;
+
         }
     }
 }
