e530369ba61584052e4710b88b47719cc5892755
sedat <sedatkarasungur4@gmail.com>
2021-04-03 16:20:08 +0300

    TLP Bitti ve PLINQ Başlıyor

---
+++ TLPForEachParallelFormApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TLPForEachParallelFormApp
+{
+    public partial class Form1 : Form
+    {
+        public int Counter { get; set; } = 0;
+        CancellationTokenSource CancellationToken = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Getir ve Başlat
+        {
+            List<string> urls = new List<string>() {
+            "https://www.google.com",
+            "https://www.microsoft.com",
+            "https://www.amazon.com",
+            "https://www.amazon.com",
+            "https://www.google.com",
+            "https://www.amazon.com",
+            "https://www.google.com",
+             "https://www.amazon.com",
+            "https://www.amazon.com",
+            "https://www.google.com",
+            "https://www.amazon.com",
+            "https://www.google.com"
+
+            };
+
+
+            HttpClient client = new HttpClient();
+
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = CancellationToken.Token;
+
+
+            Task.Run(() =>
+            {//içerideki işlemler farklı bir threadda çalışsın
+
+                try
+                {
+
+                    Parallel.ForEach<string>(urls, parallelOptions, (gelenUrl) =>//parallelOptions kullanım sebebi Global CancellationToken erişemediğimiz durumda parallelOptions.CancellationToken.ThrowIfCancellationRequested ile erişmek için 
+                    {
+                        string content = client.GetStringAsync(gelenUrl).Result;//gelen hataları handle edbilmek için Result kullandık
+
+                        string data = $"{gelenUrl}: {content.Length}";//url ve değer sayısını gösterelim listboxta
+
+
+                        CancellationToken.Token.ThrowIfCancellationRequested();//işlemi iptal ettiğimizde hataya düşsün diye ekledik
+
+                        listBox1.Invoke((MethodInvoker)delegate//farklı bir threadda ekleme işlemini yapsın diye kullandık
+                        {
+                            listBox1.Items.Add(data);
+                        });
+
+                    });
+                }
+                catch (OperationCanceledException ex)
+                {
+                    MessageBox.Show("İşlem İptal Edildi"+ex.Message);
+                }
+
+            });
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//arttır
+        {
+            button2.Text = Counter++.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)//İptal
+        {
+            CancellationToken.Cancel();
+
+        }
+    }
+}
---
+++ TLPForeachParallelApp/Program.cs
@@ -0,0 +1,125 @@
+﻿
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TLPForeachParallelApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();//işlemlerde süreleri görmek için kullanabilirsin. Genel Kültür olarak kalsın
+
+            stopwatch.Start();
+            string picsPath = @"C:\Users\limit\Desktop\Resimler"; // \ işaretinde hata vermesin diye başına @ işareti konuldu Burada masaüstümüzde bulunan resim klasörünün yolunu verdik
+
+            var files = Directory.GetFiles(picsPath); //string[] dizisi alır
+
+            Parallel.ForEach(files,(resimNesnesi) => {//Bununla beraber bir işlem için çoklu thread kullanarak özellikle uzun işlemlerde süreyi kısaltabiliriz kısa işlemler de senkrona göre daha yavaş olabilir. Bu yüzden kullandığımız yerleri iyi seçmeliyiz. Mesela bu işlem de multithread senkrondan daha yavaş çalışır Ama daha büyük işlemlerde daha hızlı olur. Bildiğimiz foreachtan farkı yok
+
+                Console.WriteLine("Thread No"+Thread.CurrentThread.ManagedThreadId);
+                Image ımage = new Bitmap(resimNesnesi);//Bitmap imagedan kalıtım alır ondan nesne oluşturduk genel kültür olarak aklında kalsın
+
+                var kucukResim = ımage.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+                kucukResim.Save(Path.Combine(picsPath, "KucukResim", Path.GetFileName(resimNesnesi)));
+
+            });
+
+   
+            stopwatch.Stop();
+
+            Console.WriteLine("İşlem Bitti ve süresi : "+stopwatch.ElapsedMilliseconds);
+
+
+            ///////////////////////////////////////////// 2. Örnek RaceCondition olmasını engelleme  ////////////////////////////////////////////////////////////
+
+            long FilesByte = 0;
+
+            string picsPath2 = @"C:\Users\limit\Desktop\Resimler";
+
+            var files2 = Directory.GetFiles(picsPath2);
+
+            Parallel.ForEach(files2, (resimNesnesi2) => {
+
+                Console.WriteLine("Thread No" + Thread.CurrentThread.ManagedThreadId);
+
+                FileInfo f = new FileInfo(resimNesnesi2);
+
+
+                Interlocked.Add(ref FilesByte, f.Length);//bu Class metodunu kullanmazsak birden fazla thread aynı işlemi yabilecek ihtimali bu işlem için gereksiz bir kullanım olacak Buna RaceCondition denir. Burada resimlerin ana dosyasının dosya boyutunu yanlış hesaplayacak. Ama bu class bir thread işlemini yapmadan diğer threada izin vermez belki lock yaratır ama tutarsızlığı engeller ve düzgün bir arttırım olur azaltma işlemi için  Interlocked.Decrement değiştirmek için ise Interlocked.Exchange metodu güvenlidir.
+
+
+            });
+
+
+            //////////////////////////////////////////////3.Örnek  RaceCondition Örnek ////////////////////////////////////////////////////////////////////
+
+            int deger = 0;
+
+            Parallel.ForEach(Enumerable.Range(1,100000).ToList(),(sayi)=> {
+
+                deger = sayi;
+            
+            });
+
+            Console.WriteLine(deger);//burada 100000 değeri en son olacağı için onu vermesi gerekiyor ama bazen RaceCondition durumu oluşur ve farklı bir sayı ekrana verebilir
+
+
+            ////////////////////////////////////////////4.Örnek  Parallel.For örnek/////////////////////////////////////////////////////////////////////////////////////
+
+
+            long totalByte = 0;
+
+
+            string picsPath3 = @"C:\Users\limit\Desktop\Resimler";
+
+            var files3 = Directory.GetFiles(picsPath3);
+
+            Parallel.For(0,files3.Length, (index) => {
+
+
+               var file= new FileInfo(files3[index]);
+
+                Interlocked.Add(ref totalByte, file.Length);//Race Condition olmasın diye bunu kullanırız
+
+
+            });
+
+
+            ////////////////////////////////////////////5.Örnek  Parallel.Foreach Daha Performanslı örnek/////////////////////////////////////////////////////////////////////////////////////
+
+
+            long Total = 0;
+
+
+
+            Parallel.ForEach(Enumerable.Range(1, 100000).ToList(),()=>0, (gelenSayi,dongu, threadIslemToplamı) => {//Foreach ile kullanımı her bir thread kendi atandığı sayıları topladıktan sonra Interlocked eklensin
+
+                threadIslemToplamı += gelenSayi;
+
+                return threadIslemToplamı;
+
+            },(threadIslemToplamıSonucu) =>Interlocked.Add(ref Total, threadIslemToplamıSonucu));
+
+            ////////////////////////////////////////////6.Örnek  Parallel.For Daha Performanslı örnek/////////////////////////////////////////////////////////////////////////////////////
+            
+
+            Parallel.For(0,100, () => 0, (gelenSayi, dongu, threadIslemToplamı) => {//For ile kullanımı her bir thread kendi atandığı sayıları topladıktan sonra Interlocked eklensin
+
+                threadIslemToplamı += gelenSayi;
+
+                return threadIslemToplamı;
+
+            }, (threadIslemToplamıSonucu) => Interlocked.Add(ref Total, threadIslemToplamıSonucu));
+
+
+        }
+    }
+}
